11ac0b5c83130b54d8475bca8e86b51f68519a0a
Shu Yan <yanshu.usc@gmail.com>
2018-07-12 16:44:47 -0400

    add error msg

--- weibopy/error.py
+++ weibopy/error.py
@@ -2,4 +2,12 @@
 class APIError(Exception):
 
     def __init__(self, response: dict):
-        self.message = response.get('error', 'API error')
+        self.error = response.get('error', 'API error')
+        self.error_code = response.get('error_code', 1)
+        self.request = response.get('request', '')
+
+    def __str__(self):
+        return '{} ({})'.format(self.error, self.error_code)
+
+    def __repr__(self):
+        return 'Error: {} Code: {}'.format(self.error, self.error_code)
